6ad8e568a36a802f1e86d722feeb565d0fd29d53
github shi0ge3 <katsurashigeki0325@gmail.com>
2021-03-28 22:33:17 +0900

    テスト駆動：No706(14％)
    MoneyクラスにDollarクラスのequalsメソッドを引き上げ(スーパークラスに移動)

--- JavaVer/src/money/Dollar.java
+++ JavaVer/src/money/Dollar.java
@@ -1,18 +1,11 @@
 package money;
 
 public class Dollar extends Money {
-	private int amount;
-
 	Dollar(int amount) {
 		this.amount = amount;
 	}
 
 	Dollar times(int multiplier) {
 		return new Dollar(amount * multiplier);
-	}
-
-	public boolean equals(Object object) {
-		Dollar dollar = (Dollar)object;
-		return amount == dollar.amount;
 	}
 }
