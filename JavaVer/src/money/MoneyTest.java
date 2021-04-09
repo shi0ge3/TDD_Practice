54d0c13426f9d83898fbd7bbadd680ccb5aa5dd3
github shi0ge3 <katsurashigeki0325@gmail.com>
2021-04-10 00:06:53 +0900

    テスト駆動：No.1495(29％)
    通貨の足し算機能追加、開始。
    テストケース作成して、それを満たすためにインターフェース・クラスを作成。
    テスト失敗。
    
    疑問点
    インターフェースが考えることが少ないとはなぜだろうか？
    Bankはクラスで作成したのは何を基準にしてクラスとインターフェースなのだろうか？
    テストを失敗したが、なぜ前に進んでいると今回は言えるのだろうか？

--- JavaVer/src/money/MoneyTest.java
+++ JavaVer/src/money/MoneyTest.java
@@ -26,6 +26,8 @@ class MoneyTest {
 	}
 	@Test
 	public void testSimpleAddition() {
+		Money five = Money.dollar(5);
+		Expression sum = five.plus(five);
 		Bank bank = new Bank();
 		Money reduced = bank.reduce(sum, "USD");
 		assertEquals(Money.dollar(10), reduced);
