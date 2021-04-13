b77baa47d28f211af2ac5ff405586bdb7816bcd4
github shi0ge3 <katsurashigeki0325@gmail.com>
2021-04-13 23:12:14 +0900

    テスト駆動：No.1764(33％)
    Expressionインターフェースに引き上げ成功。
    テスト成功。
    
    疑問点
    計算をする際に絶対実装させたいメソッドが今回のreduceなのだろうか？

--- JavaVer/src/money/Bank.java
+++ JavaVer/src/money/Bank.java
@@ -2,9 +2,6 @@ package money;
 
 class Bank {
 	Money reduce(Expression source, String to) {
-		if(source instanceof Money)
-			return (Money) source;
-		Sum sum = (Sum) source;
-		return sum.reduce(to);
+		return source.reduce(to);
 	}
 }
