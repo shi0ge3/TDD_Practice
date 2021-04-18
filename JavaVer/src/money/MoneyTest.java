2ba79fdad7e1dfa337e64e0536d270a257557c6e
github shi0ge3 <katsurashigeki0325@gmail.com>
2021-04-18 23:34:01 +0900

    テスト駆動：No.1977(37％)

--- JavaVer/src/money/MoneyTest.java
+++ JavaVer/src/money/MoneyTest.java
@@ -64,4 +64,13 @@ class MoneyTest {
 	public void testIdentiyRate() {
 		assertEquals(1, new Bank().rate("USD", "USD"));
 	}
+	@Test
+	public void testMixedAddtion() {
+		Money fiveBucks = Money.dollar(5);
+		Money tenFrancs = Money.franc(10);
+		Bank bank = new Bank();
+		bank.addRate("CHF", "USD", 2);
+		Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+		assertEquals(Money.dollar(10), result);
+	}
  }
