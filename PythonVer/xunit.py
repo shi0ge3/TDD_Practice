397fd16fe0a758b663fd9910a3bfbe1c14384253
github shi0ge3 <katsurashigeki0325@gmail.com>
2021-04-25 11:16:23 +0900

    テスト駆動開発（第２部４７％）
    TestCaseを引き上げ完了。
    テストは成功。

--- PythonVer/xunit.py
+++ PythonVer/xunit.py
@@ -1,17 +1,16 @@
 class TestCase:
-    pass
-
-
-class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         self.name = name
-
 
     def run(self):
         method = getattr(self, self.name)
         method()
 
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        super().__init__(name)
 
     def testMethod(self):
         self.wasRun = 1
