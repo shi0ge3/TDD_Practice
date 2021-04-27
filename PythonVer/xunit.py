d3b4f9bb49d1154ab202370a90fb51639497c2f3
github shi0ge3 <katsurashigeki0325@gmail.com>
2021-04-27 23:29:57 +0900

    テスト駆動開発（第２部５０％）
    TestCaseTestクラスの簡素化。
    テストは成功。
    
    疑問点
    なぜこのようなコードを重いつたのか？

--- PythonVer/xunit.py
+++ PythonVer/xunit.py
@@ -12,11 +12,8 @@ class TestCase:
 
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
-
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
 
     def testMethod(self):
@@ -24,16 +21,16 @@ class WasRun(TestCase):
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
 
     def testSetUp(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert(test.wasSetUp)
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 
 TestCaseTest("testRunning").run()
