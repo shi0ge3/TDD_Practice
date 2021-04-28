11dacc2da8e9a96e6e6af7e0af4292d46b82cf21
github shi0ge3 <katsurashigeki0325@gmail.com>
2021-04-28 23:48:17 +0900

    テスト駆動開発（第２部５０％）
    WasRunクラスにログを保存する機能を追加。
    テストは成功。
    
    疑問点
    リソース解放をテストする追加とは？
    フラグがなぜ急にでてきたのか？

--- PythonVer/xunit.py
+++ PythonVer/xunit.py
@@ -15,6 +15,7 @@ class WasRun(TestCase):
     def setUp(self):
         self.wasRun = None
         self.wasSetUp = 1
+        self.log = "setUp"
 
     def testMethod(self):
         self.wasRun = 1
@@ -30,7 +31,7 @@ class TestCaseTest(TestCase):
 
     def testSetUp(self):
         self.test.run()
-        assert(self.test.wasSetUp)
+        assert("setUp" == self.test.log)
 
 
 TestCaseTest("testRunning").run()
