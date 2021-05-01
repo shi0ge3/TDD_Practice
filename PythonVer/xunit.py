0196e48536e69d7cd961c5901f75fb8742b53be4
shi0ge3 <katsurashigeki0325@gmail.com>
2021-05-01 18:12:05 +0900

    テスト駆動開発（第２部５１％）
    TestCaseTestクラスのsetUp関数を削除。
    testTemplateMethod関数の処理を変更。
    テストは失敗。
    
    疑問点
    テスト失敗時の関数準備が整ったみたいだが、
    どんなことをこれからやるのだろうか？

--- PythonVer/xunit.py
+++ PythonVer/xunit.py
@@ -20,12 +20,9 @@ class WasRun(TestCase):
 
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
     def testTemplateMethod(self):
-        self.test.run()
-        assert("setUp testMethod " == self.test.log)
+        test = WasRun("testMethod")
+        assert("setUp testMethod " == test.log)
 
 
 TestCaseTest("testTemplateMethod").run()
