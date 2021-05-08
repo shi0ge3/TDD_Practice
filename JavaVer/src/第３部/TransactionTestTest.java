c8ef87e7df2636e70e8dd1b5957f529dcb1f116a
shi0ge3 <katsurashigeki0325@gmail.com>
2021-05-08 23:59:30 +0900

    テスト駆動開発（第３部６２％）
    アサーションの例を記述。
    ソケット通信の再現。
    ソケットとサーバを接続する場面を記述。
    テストは失敗。

--- JavaVer/src/第３部/TransactionTestTest.java
+++ JavaVer/src/第３部/TransactionTestTest.java
@@ -2,12 +2,17 @@ package 第３部;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.net.Socket;
+import java.nio.Buffer;
+
 import org.junit.jupiter.api.Test;
 
 class TransactionTestTest {
 
 	@Test
 	void testCompleteTransaction(){
+		Socket reader = new Socket("localhost", defaultPort());
+		Buffer reply = reader.contents();
 		assertTrue(reader.isClosed);
 		assertEquals("abc", reply.contents());
 	}
