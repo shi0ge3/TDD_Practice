de90d0b259ccaaadad733c04ee87703754935419
shi0ge3 <katsurashigeki0325@gmail.com>
2021-05-09 00:02:05 +0900

    テスト駆動開発（第３部６２％）
    アサートファーストの例を記述。
    Serverが必要なため追記。
    テストは失敗。

--- JavaVer/src/第３部/TransactionTestTest.java
+++ JavaVer/src/第３部/TransactionTestTest.java
@@ -7,10 +7,13 @@ import java.nio.Buffer;
 
 import org.junit.jupiter.api.Test;
 
+import com.sun.security.ntlm.Server;
+
 class TransactionTestTest {
 
 	@Test
 	void testCompleteTransaction(){
+		Server writer = new Server(defaultPort(), "abc");
 		Socket reader = new Socket("localhost", defaultPort());
 		Buffer reply = reader.contents();
 		assertTrue(reader.isClosed);
