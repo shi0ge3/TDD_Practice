90b1800a1bb0fca69531c0592da853968b085c5f
shi0ge3 <katsurashigeki0325@gmail.com>
2021-05-17 08:57:23 +0900

    テスト駆動開発（第２部６７％）
    Log Stringパターンの実例。
    順序良くテスト実行を行い、
    検知されてないかを確認するObsesrverパターンの一例。

---
+++ PythonVer/TemplateMethodTest.py
@@ -0,0 +1,18 @@
+class TemplateMethodTest(object):
+    def testTmplateMethod(self):
+        test = WasRun("testMethod")
+        result = TestResult()
+        test.run(result)
+        assert("setUp testMethod tear Down " == test.log)
+
+class WasRun(object):
+    def setUp(self):
+        self.log = "setUp "
+
+
+    def testMethod(self):
+        self.log = self.log + "testMethod "
+
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
