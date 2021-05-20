69d05ae793b76a38d77b632a05b74a6ec2f6b10e
shi0ge3 <katsurashigeki0325@gmail.com>
2021-05-21 08:50:18 +0900

    テスト駆動開発（第３部６９％）
    三角測量の実例。
    テストケースからパスするメソッドを記述していく。

---
+++ JavaVer/第３部/CalcTest.java
@@ -0,0 +1,17 @@
+package 第３部;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalcTest {
+
+	@Test
+	void test() {
+		assertEquals(4, plus(3, 1));
+	}
+
+	private int plus(int agend, int addend) {
+		return 4;
+	}
+}
