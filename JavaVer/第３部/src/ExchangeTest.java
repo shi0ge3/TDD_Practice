944df51aaf766bb5e4f84d0ee14ae43f33b8bb82
shi0ge3 <katsurashigeki0325@gmail.com>
2021-05-24 08:46:01 +0900

    第３部:第２９章_xUnitのパターン(７３％)
    例外のテストの例。
    値の検索を失敗して例外発生するかのテストを作成。
    対象メソッドの直下にfail()を使用すること。
    それを行うことで、例外テストをうまく実施できたか、
    わかるため。

--- JavaVer/第３部/src/ExchangeTest.java
+++ JavaVer/第３部/src/ExchangeTest.java
@@ -1,13 +1,24 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class ExchangeTest {
     @Test
     public void testRate() {
-        Object exchange;
+        Object exchange = null;
         exchange.addRate("USD", "GBP", 2);
         int rate = exchange.findRate("USD", "GBP");
         assertEquals(2, rate);
+    }
+}
+@Test
+public void testMissingRate() {
+    try {
+        Object exchange = null;
+        exchange.findRate("USD", "GBP");
+        fail();
+    } catch (IllegalArgumentException expected) {
+
     }
 }
