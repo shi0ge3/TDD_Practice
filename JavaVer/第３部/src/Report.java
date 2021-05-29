9913c91ffa93cda22631902861a057c2619253e8
shi0ge3 <katsurashigeki0325@gmail.com>
2021-05-29 18:19:24 +0900

    第３部:第３０章_デザインパターン(７７％)
    PluggableSelectorパターンの例。
    インスタンス毎に異なるメソッドの動きを実装したい時に使う。パターン適用前。
    これからswitch文を取り除く。

--- JavaVer/第３部/src/Report.java
+++ JavaVer/第３部/src/Report.java
@@ -1,3 +1,22 @@
 abstract class Report {
-    abstract void print();
+    String printMessage;
+
+    Report(String printMessage) {
+        this.printMessage = printMessage;
+    }
+
+    void print() {
+        switch (printMessage) {
+            case "printHTML":
+                printHTML();
+                break;
+            case "printXML":
+                printXML();
+                break;
+        }
+    }
+
+    void printHTML(){}
+
+    void printXML(){}
 }
