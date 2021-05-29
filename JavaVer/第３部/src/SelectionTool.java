e1ab6658439a6ae67a120a5e08b7d0ed5352bbd8
shi0ge3 <katsurashigeki0325@gmail.com>
2021-05-29 18:34:27 +0900

    ちょっとだけコードをコンパイル通過のため、 リファクタリング。静的解析のエラー解除成功。

--- JavaVer/第３部/src/SelectionTool.java
+++ JavaVer/第３部/src/SelectionTool.java
@@ -1,5 +1,3 @@
-import javax.swing.*;
-
 public class SelectionTool {
     Figure selected;
     SelectionMode mode;
@@ -10,10 +8,36 @@ public class SelectionTool {
         else
             mode = new MultipleSelection();
     }
+
+    private Figure findFigre() {
+        return new Figure();
+    }
+
     public void mouseMove() {
         mode.mouseMove();
     }
     public void mouseUp() {
         mode.mousuUp();
+    }
+}
+
+class Figure {
+}
+
+class SelectionMode {
+    void mousuUp() {
+    }
+
+    void mouseMove() {
+    }
+}
+
+class SingleSelectionModel extends SelectionMode {
+    SingleSelectionModel(Figure selected) {
+    }
+}
+
+class MultipleSelection extends SelectionMode {
+    MultipleSelection(){
     }
 }
