4aba752704da4283528aba8f0398bf7729d58648
shi0ge3 <katsurashigeki0325@gmail.com>
2021-05-31 23:24:47 +0900

    第３部:第３０章_デザインパターン(７８％)
    Imposterパターンの例。
    既存のオブジェクトを使用して新しいテストシナリオを作成できな場合。
    パターン適用前。

---
+++ JavaVer/第３部/src/DrawingTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DrawingTest {
+    @Test
+    public void testRectangle() {
+        Drawing d = new Drawing();
+        d.addFigure(new RectangleFigure());
+        RecordingMedium brush = new RecordingMedium();
+        d.display(brush);
+        assertEquals("rectangle 0 10 50 100\n", brush.log())
+    }
+}
+
+class Drawing {
+    public void addFigure(RectangleFigure rectangleFigure) {
+    }
+
+    public void display(RecordingMedium brush) {
+    }
+}
+
+class RectangleFigure {
+}
+
+class RecordingMedium {
+
+    public String log() {
+    }
+}
