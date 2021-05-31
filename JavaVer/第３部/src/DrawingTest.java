2932c9d352fb9dd5dd0280cfff6233b9c5ce96cc
shi0ge3 <katsurashigeki0325@gmail.com>
2021-06-01 08:39:01 +0900

    第３部:第３０章_デザインパターン(７８％)
    Imposterパターンの例。
    既存のオブジェクトを使用して新しいテストシナリオを作成できな場合。
    パターン適用後。一部修正。
    OvalFigureに修正。同じ振る舞いに可能になった。

--- JavaVer/第３部/src/DrawingTest.java
+++ JavaVer/第３部/src/DrawingTest.java
@@ -6,12 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DrawingTest {
     @Test
-    public void testRectangle() {
+    public void testOval() {
         Drawing d = new Drawing();
-        d.addFigure(new RectangleFigure());
+        d.addFigure(new OvalFigure(0, 10, 50, 100));
         RecordingMedium brush = new RecordingMedium();
         d.display(brush);
-        assertEquals("rectangle 0 10 50 100\n", brush.log());
+        assertEquals("oval 0 10 50 100\n", brush.log());
     }
 }
 
@@ -21,9 +21,23 @@ class Drawing {
 
     public void display(RecordingMedium brush) {
     }
+
+    public void addFigure(OvalFigure ovalFigure) {
+    }
 }
 
 class RectangleFigure {
+}
+
+class OvalFigure {
+    int x, y, h, w;
+
+    public OvalFigure(int x, int y, int h, int w) {
+        this.x = x;
+        this.y = y;
+        this.h = h;
+        this.w = w;
+    }
 }
 
 class RecordingMedium {
