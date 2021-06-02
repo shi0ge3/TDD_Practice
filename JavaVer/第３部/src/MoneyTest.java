0e271eb7a5023194e1d176cf77e053977025a4a9
shi0ge3 <katsurashigeki0325@gmail.com>
2021-06-03 08:36:30 +0900

    第３部:第３０章_デザインパターン(７９％)
    Collecting Parameterパターンの例。
    複数のオブジェクトの処理結果を集めるには。
    例として計算結果を出力する機能を開発中。

--- JavaVer/第３部/src/MoneyTest.java
+++ JavaVer/第３部/src/MoneyTest.java
@@ -9,6 +9,11 @@ public class MoneyTest {
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
+    @Test
+    public void testSumPrinting() {
+        Sum sum = new Sum(Money.dollar(5), Money.franc(7));
+        assertEquals("5 USD + 7 CHF", sum.toString());
+    }
 }
 
 class Money {
@@ -23,8 +28,22 @@ class Money {
         return new Money();
     }
 
+    public static Object franc(int i) {
+        return new Object();
+    }
+
     public Money plus(Money balance) {
         return new Money();
+    }
+}
+
+class Sum {
+    private String augend, addend;
+
+    Sum(Object dollar, Object franc){}
+
+    public String toString() {
+        return augend + " + " + addend;
     }
 }
 
