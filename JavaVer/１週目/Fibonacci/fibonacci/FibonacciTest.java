ddc3d212b9a65372d39c4abe4a7cb48d96801783
shi0ge3 <katsurashigeki0325@gmail.com>
2021-06-11 08:50:28 +0900

    付録B:フィボナッチ(９０％)
    フィボナッチ処理。
    一般化の記述を行う。
    フィボナッチは回帰処理。
    fibメソッドの処理を修正、テスト駆動のフィボ完成。
    テスト失敗。
    
    疑問点
    わざと２の場合は１を返しているのだろうか？

--- JavaVer/１週目/Fibonacci/fibonacci/FibonacciTest.java
+++ JavaVer/１週目/Fibonacci/fibonacci/FibonacciTest.java
@@ -6,8 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FibonacciTest {
     int fib(int n) {
         if(n == 0) return 0;
-        if(n <= 2) return 1;
-        return 1 + 1;
+        if(n == 1) return 1;
+        return fib(n-1) + fib(n-2);
     }
     @Test
     public void testFibonacci() {
