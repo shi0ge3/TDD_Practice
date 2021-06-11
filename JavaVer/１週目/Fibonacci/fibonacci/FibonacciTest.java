41f20a8ac23c41b8165a15628a88c0de79975ab1
shi0ge3 <katsurashigeki0325@gmail.com>
2021-06-12 00:37:52 +0900

    付録B:フィボナッチ(１００％)
    フィボナッチ処理。
    一般化の記述を行う。
    フィボナッチは回帰処理。
    fibメソッドの処理を修正、テスト駆動のフィボ完成。
    テスト成功。

--- JavaVer/１週目/Fibonacci/fibonacci/FibonacciTest.java
+++ JavaVer/１週目/Fibonacci/fibonacci/FibonacciTest.java
@@ -13,6 +13,6 @@ public class FibonacciTest {
     public void testFibonacci() {
         int cases[][] = {{0,0},{1,1},{2,1},{3,2}};
         for (int i = 0; i < cases.length; i++)
-            assertEquals(cases[i][1], fib(cases[i][1]));
+            assertEquals(cases[i][1], fib(cases[i][0]));
     }
 }
