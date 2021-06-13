5e3e1a631a70279d5b89d1840f32cfc873e03635
shi0ge3 <katsurashigeki0325@gmail.com>
2021-06-13 11:24:50 +0900

    第一部:第２章 明白な実装(９％)
    多国通貨を実装を通して学習。
    Value Objectパターンを使用。
    説明(７５％)は読了。
    TODOリストを追記。
    equalsメソッドのテストを追記。
    
    テスト結果はレッド。
    
    疑問点
    hashCodeメソッドがなぜ必要なのだろうか？

--- JavaVer/２週目/第１部/money/MoneyTest.java
+++ JavaVer/２週目/第１部/money/MoneyTest.java
@@ -4,6 +4,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/*
+* TODO
+* $5 + 10 CHF = $10(レートが２：１の場合)
+* amount を private にする。
+* Money の丸め処理をどうする？
+* equals()
+* hashCode()
+*/
+
 public class MoneyTest {
     @Test
     public void testMultiplication() {
@@ -12,5 +21,9 @@ public class MoneyTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
     }
 }
