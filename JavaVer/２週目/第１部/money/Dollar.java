f5973c0deb254ede345537e02c7cbee337b79e89
shi0ge3 <katsurashigeki0325@gmail.com>
2021-06-17 08:43:18 +0900

    第一部:第６章 テスト不足に気づいたら(１４％)
    多国通貨を実装を通して学習。
    equalsメソッドの引数もMoney型に変更
    
    テスト結果はグリーン。
    
    疑問点

--- JavaVer/２週目/第１部/money/Dollar.java
+++ JavaVer/２週目/第１部/money/Dollar.java
@@ -10,7 +10,7 @@ class Dollar extends Money {
     }
 
     public boolean equals(Object object) {
-        Money dollar = (Dollar) object;
+        Money dollar = (Money) object;
         return amount == dollar.amount;
     }
 }
