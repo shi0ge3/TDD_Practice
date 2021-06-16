03de1528b86f10de1ea53d468337877b3871e50f
shi0ge3 <katsurashigeki0325@gmail.com>
2021-06-17 08:46:49 +0900

    第一部:第６章 テスト不足に気づいたら(１４％)
    多国通貨を実装を通して学習。
    equalsメソッドdollar→moneyに変更。
    親クラスMoneyに引き上げ(移動)。
    
    テスト結果はグリーン。
    
    疑問点
    細かくステップとテスト実行で進行。
    これが自信を持ちながらプログラミングが
    行えるということなのだろうか？

--- JavaVer/２週目/第１部/money/Dollar.java
+++ JavaVer/２週目/第１部/money/Dollar.java
@@ -8,9 +8,4 @@ class Dollar extends Money {
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
-
-    public boolean equals(Object object) {
-        Money dollar = (Money) object;
-        return amount == dollar.amount;
-    }
 }
