a8cc3e0d86b714da89f16527cddee3c0793123a8
shi0ge3 <katsurashigeki0325@gmail.com>
2021-06-19 13:43:09 +0900

    第一部:第７章 疑念をテストに翻訳する(１６％)
    多国通貨を実装を通して学習。
    通過の単位が異なる場合のテストケースを追加。
    
    テスト結果は失敗。
    
    疑問点
    Moneyクラスを継承した子クラスが違うとは、
    どういたった記述で対処するのだろうか？

--- JavaVer/２週目/第１部/money/MoneyTest.java
+++ JavaVer/２週目/第１部/money/MoneyTest.java
@@ -30,6 +30,7 @@ public class MoneyTest {
 
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollar(5)));
     }
     @Test
     public void testFrancMultiplication() {
