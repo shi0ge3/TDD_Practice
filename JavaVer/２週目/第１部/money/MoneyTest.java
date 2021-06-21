ea1de18be3a11388b60a6fa2981eb478f584e2a8
shi0ge3 <katsurashigeki0325@gmail.com>
2021-06-22 08:48:02 +0900

    第一部:第９章 歩幅の調整(２４％)
    多国通貨を実装を通して学習。
    テストコードを追記。
    
    テスト結果は失敗。
    
    疑問点
    これから、親クラス子クラスを同じ通貨として、
    表現するのはどうすればできるだろうか？

--- JavaVer/２週目/第１部/money/MoneyTest.java
+++ JavaVer/２週目/第１部/money/MoneyTest.java
@@ -44,4 +44,8 @@ public class MoneyTest {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
     }
+    @Test
+    public void testDifferentClassEqualiry() {
+        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
+    }
 }
