1bdeaf3437ed45aacd625157c5f5841c0d5dae40
shi0ge3 <katsurashigeki0325@gmail.com>
2021-07-07 00:15:33 +0900

    第一部:第１８章 xUnitへ向かう小さな一歩(４７％)
    xUnitをPythonで作成して学習。
    親クラスを作成して、引き上げ。
    
    テスト通過。
    
    疑問点
    次はどの内容を引き上げるのだろうか？

--- PythonVer/２週目/第２部/xunit/xunit.py
+++ PythonVer/２週目/第２部/xunit/xunit.py
@@ -1,11 +1,12 @@
 class TestCase(object):
-    pass
+    def __init__(self, name):
+        self.name = name
 
 
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
-        self.name = name
+        super().__init__(name)
 
     def run(self):
         method = getattr(self, self.name)
