24e225a52c385311c6ec780258d441d9a9bd460e
shi0ge3 <katsurashigeki0325@gmail.com>
2021-07-08 00:32:25 +0900

    第一部:第１９章 前準備(４９％)
    xUnitをPythonで作成して学習。
    TestCaseTestクラスに新たなテストケースを追記。
    
    テスト失敗。
    
    疑問点
    共通オブジェクトをテストの前準備生成する処理はどの様な内容だろうか？

--- PythonVer/２週目/第２部/xunit/xunit.py
+++ PythonVer/２週目/第２部/xunit/xunit.py
@@ -1,3 +1,12 @@
+"""
+TODO
+setUpを最初に呼び出す
+tearDownを後で呼び出す
+テストメソッドが失敗したとしてもtearDownを呼び出す
+複数のテストを走らせる
+収集したテスト結果を出力する
+"""
+
 class TestCase(object):
     def __init__(self, name):
         self.name = name
@@ -23,5 +32,11 @@ class TestCaseTest(TestCase):
         test.run()
         assert(test.wasRun)
 
+    def testSetUp(self):
+        test = WasRun("testMthod")
+        test.run()
+        assert(test.wasSetUp)
+
 
 print(TestCaseTest("testRunning").run())
+print(TestCaseTest("testSetUp").run())
