ca6d318a3cc75f4c5a0badf6314b3d06da69af36
shi0ge3 <katsurashigeki0325@gmail.com>
2021-07-08 00:38:42 +0900

    第一部:第１９章 前準備(４９％)
    xUnitをPythonで作成して学習。
    WasRunクラスをリファクタリング。
    
    テスト失敗。
    
    疑問点
    共通オブジェクトをテストの前準備生成する処理はどの様な内容だろうか？

--- PythonVer/２週目/第２部/xunit/xunit.py
+++ PythonVer/２週目/第２部/xunit/xunit.py
@@ -22,11 +22,8 @@ class TestCase(object):
 
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
-
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
 
     def testMethod(self):
