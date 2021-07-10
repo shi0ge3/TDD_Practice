95adea0ab1920f0fb2c9487f9e03a32a7be833f8
shi0ge3 <katsurashigeki0325@gmail.com>
2021-07-10 10:57:04 +0900

    第一部:第２０章 後片付け(５１％)
    xUnitをPythonで作成して学習。
    不要なテストケースを削除。
    WasRunクラスの不要部分を削除。
    
    テスト失敗。
    
    疑問点

--- PythonVer/２週目/第２部/xunit/xunit.py
+++ PythonVer/２週目/第２部/xunit/xunit.py
@@ -4,7 +4,6 @@ tearDownを後で呼び出す
 テストメソッドが失敗したとしてもtearDownを呼び出す
 複数のテストを走らせる
 収集したテスト結果を出力する
-WasRunで文字列をログに記録する
 """
 
 
@@ -23,21 +22,17 @@ class TestCase(object):
 
 class WasRun(TestCase):
     def setUp(self):
-        self.wasRun = None
         self.log = "setUp "
 
     def testMethod(self):
-        self.wasRun = 1
         self.log = self.log + "testMethod "
 
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
     def testSetUp(self):
-        self.test.run()
+        test = WasRun("testMethod")
+        test.run()
         assert("setUp testMethod " == self.test.wasSetUp)
 
 
-print(TestCaseTest("testSetUp").run())
+print(TestCaseTest("testTemplateMethod").run())
