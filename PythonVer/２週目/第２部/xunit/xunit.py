3cf773af6832a16997f502a32ad0e8968da2c414
shi0ge3 <katsurashigeki0325@gmail.com>
2021-07-12 00:18:58 +0900

    第一部:第２１章 数え上げ(５２％)
    xUnitをPythonで作成して学習。
    テストの失敗数をカウント機能を実装。
    TestCaseTestクラスにテストコードを追記。
    
    テスト失敗。
    
    疑問点

--- PythonVer/２週目/第２部/xunit/xunit.py
+++ PythonVer/２週目/第２部/xunit/xunit.py
@@ -34,7 +34,7 @@ class TestCase(object):
         method = getattr(self, self.name)
         method()
         self.tearDown()
-        return TestResult()
+        return result
 
 
 class WasRun(TestCase):
@@ -59,6 +59,12 @@ class TestCaseTest(TestCase):
         result = test.run()
         assert("1 run, 0 failed" == result.summary())
 
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
+
 
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
+TestCaseTest("testFailedResult").run()
