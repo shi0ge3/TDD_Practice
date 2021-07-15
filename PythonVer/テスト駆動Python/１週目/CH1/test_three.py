7ee19a8df2a8188de63a053fb78c18b69b80facc
shi0ge3 <katsurashigeki0325@gmail.com>
2021-07-16 00:31:42 +0900

    第１週目　CH１　テスト駆動Python(８％)
    Taskクラスを対象としたpytestファイルを作成。
    一部、間違い箇所の修正済み。
    
    疑問
    他にどの様な機能があるのだろうか？

--- PythonVer/テスト駆動Python/１週目/CH1/test_three.py
+++ PythonVer/テスト駆動Python/１週目/CH1/test_three.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
 
-Task = namedtuple('Task', ['summary', 'owner', 'id'])
+Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
 Task.__new__.__defaults__ = (None, None, False, None)
 
 
