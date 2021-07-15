2ae07ac70c5bc717c5adbb26e448aef3e2468e5e
shi0ge3 <katsurashigeki0325@gmail.com>
2021-07-15 11:50:12 +0900

    第一部:第２３章 スイートにまとめる(５７％)       第２４章 xUnitの全体ふりかえり(５８％)
    xUnitをPythonで作成して学習。
    各テストコードにTestResultオブジェクトを使用
    
    テスト成功。
    
    疑問点

--- PythonVer/２週目/第２部/xunit/xunit.py
+++ PythonVer/２週目/第２部/xunit/xunit.py
@@ -42,8 +42,6 @@ class TestCase(object):
             result.testFailed()
         self.tearDown()
 
-        return result
-
 
 class TestSuite(object):
     def __init__(self):
@@ -72,37 +70,35 @@ class WasRun(TestCase):
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.result = TestResult()
+
     def testTemplateMethod(self):
         test = WasRun("testMethod")
-        result = TestResult()
-        test.run(result)
+        test.run(self.result)
         assert("setUp testMethod tearDown " == test.log)
 
     def testResult(self):
         test = WasRun("testMethod")
-        result = TestResult()
-        test.run(result)
-        assert("1 run, 0 failed" == result.summary())
+        test.run(self.result)
+        assert("1 run, 0 failed" == self.result.summary())
 
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
-        result = TestResult()
-        test.run(result)
-        assert("1 run, 1 failed" == result.summary())
+        test.run(self.result)
+        assert("1 run, 1 failed" == self.result.summary())
 
     def testFailedResultFormatting(self):
-        result = TestResult()
         result.testStarted()
         result.testFailed()
-        assert("1 run, 1 failed" == result.summary())
+        assert("1 run, 1 failed" == self.result.summary())
 
     def testSuite(self):
         suite = TestSuite()
         suite.add(WasRun("tstMethod"))
         suite.add(WasRun("testBrokenMethod"))
-        result = suite.TestResult()
-        suite.run(result)
-        assert("2 run, 1 failed" == result.summary())
+        suite.run(self.result)
+        assert("2 run, 1 failed" == self.result.summary())
 
 
 suite = TestSuite()
